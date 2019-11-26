a8e9981b37e5b00d768c6b12846321ba8336e98b
Marc van Dijk <marcvdijk@gmail.com>
2019-11-26 11:13:11 +0100

    Restructure and update
    add package meta data

--- MDStudio_structures/__init__.py
+++ MDStudio_structures/__init__.py
@@ -1,21 +1,32 @@
 # -*- coding: utf-8 -*-
+
 """
-package:  mdstudio_structures
-LIEStudio small molecule cheminformatics functions
+MDStudio Structures service for small molecule cheminformatics functions
+
+Why import pandas here?:
+Pandas and Openbabel share a common json library name
+Adding the pandas import here avoids a REALLY NASTY race condition
+If you remove the pandas import all the HELL may break loose
 """
-# Pandas and Openbabel share a common json library name
-# Adding the pandas import here avoids a REALLY NASTY race condition
-# If you remove the pandas import all the HELL may break loose
+
 import pandas
 import os
 
-__licence__ = 'Apache Software License 2.0'
-__rootpath__ = os.path.dirname(__file__)
-
 from .cheminfo_pkgmanager import CinfonyPackageManager
+
+__module__ = 'mdstudio_structures'
+__docformat__ = 'restructuredtext'
+__version__ = '{major:d}.{minor:d}'.format(major=0, minor=2)
+__author__ = 'Marc van Dijk'
+__status__ = 'pre-release beta1'
+__date__ = '15 april 2016'
+__licence__ = 'Apache Software License 2.0'
+__url__ = 'https://github.com/MD-Studio/MDStudio_structures'
+__copyright__ = "Copyright (c) VU University, Amsterdam"
+__rootpath__ = os.path.dirname(__file__)
+__all__ = ['toolkits']
+
 
 # Load the toolkits
 paths = {}
 toolkits = CinfonyPackageManager(paths)
-
-__all__ = ['toolkits']
